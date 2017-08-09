b004f19263c20eecc2e6664bb79c18feffc0815b
shine5 <30660019+shine5@users.noreply.github.com>
2017-08-10 03:11:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,9 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 
 element.innerHTML="hi everyone";
+
+//move the image
+var img=document.getElementById('madi');
+img.onclick=function(){
+  img.style.marginleft='100px';  
+};
