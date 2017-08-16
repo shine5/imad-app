5ed60e8443ba4c8edfcc23066b7afe09dc67aae9
shine5 <30660019+shine5@users.noreply.github.com>
2017-08-17 00:35:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,23 @@
 //counter code
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick = function(){
-//make a request to the counter endpoint  
+//create a request   
+var request= new XMLHttprequest();
 
 //capture the response and store it in a variable
-
-// render the variable in the correct span
-counter=counter+1;
-var span=document.getElementById('count');
-span.innerHTML=counter.toString();
+  request.onreadystatechange=function(){
+   if (readystate===XMLHttprequest.DONE){
+       //take some action
+       if(request.status===200){
+           var counter=request.responseText;
+           var span=document.getElementById('count');
+           span.innerHTML=counter.toString();
+       }
+   } 
+   //no action   
+  };
+  //make a request
+  request.open('GET','http://fathimamaryece.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
