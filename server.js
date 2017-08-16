c223c218878aacb318057fb01460c38154db0806
shine5 <30660019+shine5@users.noreply.github.com>
2017-08-16 23:55:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter=0;
+app.get('/counter', function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 app.get('/:articlename',function(req,res){
     //articlename=== article one
     //articles[articlename]=={}content of articles
