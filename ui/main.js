9938e4625838df0979bdee981001c6525ea8750c
shine5 <30660019+shine5@users.noreply.github.com>
2017-08-17 20:37:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 //counter code
 var button=document.getElementById('counter');
-var counter=0;
+//var counter=0;
 button.onclick = function(){
 //create a request   
 var request= new XMLHttpRequest();
@@ -11,7 +11,8 @@ var request= new XMLHttpRequest();
        //take some action
        if(request.status===200){
            var counter=request.responseText;
-           counter=counter+1;
+           //counter=counter+1;
+           console.log(request.responseText);
            var span=document.getElementById('count');
            span.innerHTML=counter.toString();
        }
