c2a0e2425a264cd86e4636a56571dee8dd3fc0df
shine5 <30660019+shine5@users.noreply.github.com>
2017-08-17 20:33:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,8 +11,7 @@ var request= new XMLHttpRequest();
        //take some action
        if(request.status===200){
            var counter=request.responseText;
-        counter=counter+1;
-           var span=document.getElementById('count');
+           var span=document.getElementById('counter');
            span.innerHTML=counter.toString();
        }
    } 
